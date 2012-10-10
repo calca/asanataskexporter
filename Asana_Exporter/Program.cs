524ac62794344b24ec459b4fad81c6080b02f97e
calca <gianluigi.calcaterra@gmail.com>
2012-10-10 11:38:27 +0200

    clean repository package

--- Asana_Exporter/Program.cs
+++ Asana_Exporter/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Net;
-using Newtonsoft.Json;
 using System.Configuration;
 using Asana_Exporter_Lib.Model;
 using Asana_Exporter_Lib;
